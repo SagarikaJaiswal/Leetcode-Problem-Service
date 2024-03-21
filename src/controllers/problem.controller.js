eacf4b2f7be5cc3dd80f7a9521e0b74432bb7e3a
Sagarika Jaiswal <sagarikajaiswal97@gmail.com>
2024-03-21 20:35:52 +0530

    feat: Added routing structure for versioned api for problems

---
+++ src/controllers/problem.controller.js
@@ -0,0 +1,21 @@
+function pingProblemController(req, res) {
+  return res.json({ message: "Problem Controller is up" });
+}
+function addProblem(req, res) {}
+
+function getProblem(req, res) {}
+
+function getProblems(req, res) {}
+
+function deleteProblem(req, res) {}
+
+function updateProblem(req, res) {}
+
+module.exports = {
+  addProblem,
+  getProblem,
+  getProblems,
+  deleteProblem,
+  updateProblem,
+  pingProblemController,
+};
---
+++ src/routes/v1/problem.routes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const problemRouter = express.Router();
+const { ProblemController } = require("../../controllers");
+problemRouter.get("/ping", ProblemController.pingProblemController);
+problemRouter.post("/", ProblemController.addProblem);
+problemRouter.get("/:id", ProblemController.getProblem);
+problemRouter.get("/", ProblemController.getProblems);
+problemRouter.delete("/:id", ProblemController.deleteProblem);
+problemRouter.put("/:id", ProblemController.updateProblem);
+module.exports = problemRouter;
