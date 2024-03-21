d099e11c009c5e3a3672a833a8bbc14aec2d873e
Sagarika Jaiswal <sagarikajaiswal97@gmail.com>
2024-03-21 15:58:59 +0530

    Added body-parser

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const { PORT } = require("../src/config/server.config");
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
 app.get("/ping", (req, res) => {
   return res.json({ message: "Problem service is up" });
 });
